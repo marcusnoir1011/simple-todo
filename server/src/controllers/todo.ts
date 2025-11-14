c731887d31decced1e0a8bd127c1dd31f60b277b
Khun Min Myat <minmyatz617@gmail.com>
2025-11-14 08:53:21 +0630

    TodoList and TodoItem components added

--- server/src/controllers/todo.ts
+++ server/src/controllers/todo.ts
@@ -23,4 +23,64 @@ const createTodo = async (req: Request, res: Response) => {
     }
 };
 
-export { createTodo };
+// Getting todo
+const getAllTodo = async (req: Request, res: Response) => {
+    try {
+        const result = await Todo.find();
+
+        res.status(200).json({
+            message: "All todo has been fetched.",
+            data: result,
+        });
+    } catch (err) {
+        console.error("Error fetching all todos: ", err);
+        res.status(500).json({ message: "Something went wrong." });
+    }
+};
+const getTodo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const result = await Todo.findById(id);
+
+        res.status(200).json({
+            message: "Todo has been fetched.",
+            data: result?.title,
+        });
+    } catch (err) {
+        console.error("Error fetching the todo: ", err);
+        res.status(500).json({ message: "Something went wrong." });
+    }
+};
+
+// Update
+const updateTodo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    try {
+        const result = await Todo.findByIdAndUpdate(id, {
+            title,
+        });
+
+        res.status(200).json({
+            message: "Todo has been updated.",
+            data: result,
+        });
+    } catch (err) {
+        console.error("Error updating the todo: ", err);
+        res.status(500).json({ message: "Something went wrong." });
+    }
+};
+
+// Delete
+const deleteTodo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const result = await Todo.findByIdAndDelete(id);
+
+        res.status(200).json({ message: "Todo has been delete." });
+    } catch (err) {
+        console.error("Error deleting todo: ", err);
+        res.status(500).json({ message: "Something went wrong." });
+    }
+};
+export { createTodo, getAllTodo, getTodo, updateTodo, deleteTodo };
--- server/src/routes/todo.ts
+++ server/src/routes/todo.ts
@@ -2,8 +2,18 @@
 import { Router } from "express";
 
 // CUSTOM
-import { createTodo } from "../controllers/todo";
+import {
+    createTodo,
+    getAllTodo,
+    getTodo,
+    updateTodo,
+    deleteTodo,
+} from "../controllers/todo";
 
 const router: Router = Router();
 
 router.post("/create", createTodo);
+router.get("/allTodo", getAllTodo);
+router.get("/todo/:id", getTodo);
+router.put("/update/:id", updateTodo);
+router.delete("/delete/:id", deleteTodo);
